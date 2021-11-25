27f430a2ad95e6fb0c919e6ff48cd4b00215ada8
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-25 19:50:15 +0530

    [VIGNESH M] Added: select salary from - to retrive salary details

--- EmployeePayRollServiceDataBase.sql
+++ EmployeePayRollServiceDataBase.sql
@@ -10,7 +10,13 @@ startDate date
 ); 
 
 -----UC3-Inserting New Employee To Table-----
-INSERT INTO payroll_table(name,salary) VALUES('Dhoni',7984732.63),('Sachin',877478.63); 
+INSERT INTO payroll_table(name,salary) VALUES('Dhoni',7984732.63),('Sachin',877478.63),('Bill',976575.63); 
 
 -----UC4-Retrive All Records From Table-----
 select * from payroll_table;
+
+-----UC5-Retrive Salary Data For Particular Employee As Well As Employee Who Joined In A Particular Date Range-----
+select salary from payroll_table where name='Bill';
+select * from payroll_table where startDate between cast('2018-12-09' as date) and getDate();
+select * from payroll_table;
+
