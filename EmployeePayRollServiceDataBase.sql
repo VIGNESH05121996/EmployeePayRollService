a538a1efd2a14dc7504935914579484ab129fc58
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-25 19:32:59 +0530

    [VIGNESH M] Added: CREATE TABLE to create a table in database

--- EmployeePayRollServiceDataBase.sql
+++ EmployeePayRollServiceDataBase.sql
@@ -1,2 +1,10 @@
 -----UC1-CreateDatabase-----
 create database payroll_service;
+
+-----UC2 Create Table-----
+CREATE TABLE payroll_table(
+id int IDENTITY(1,1) PRIMARY KEY,
+name varchar(200),
+salary float,
+startDate date
+); 
